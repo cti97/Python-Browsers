5dcd5aea29ff2d25c8b3b96a9ec8c31e3c6fe497
Catalin Istratoiu <catalin@istratoiu.com>
2026-02-05 14:25:21 +0200

    removed s3 upload

--- p1/autoupdate.sh
+++ p1/autoupdate.sh
@@ -7,7 +7,7 @@ while true; do
     echo "----------------"
     python job_runner.py > ~/logs/job_runner.log 2>&1
     # upload logs to aws s3 bucket and add timestamp to filename
-    aws s3 cp ~/logs/job_runner.log s3://bitgod.xyz/logs/job_runner_$(date +%Y%m%d%H%M%S).log
+    # aws s3 cp ~/logs/job_runner.log s3://bitgod.xyz/logs/job_runner_$(date +%Y%m%d%H%M%S).log
     sleep 106
     echo "Done. Sleeping for 106 seconds."
 done
